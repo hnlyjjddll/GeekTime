36b34a88467b3e49b2222ad9018878ebe274eb3f
djangojiang <djangojiang@tencent.com>
2020-07-08 19:55:30 +0800

    单元测试

---
+++ Test/src/unit_test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by djangojiang on 2020/7/8.
+//
+
+#include <iostream>
+#include "gtest/gtest.h"
+using namespace std;
+int main(int argc, char **argv)
+{
+    cout << "This is test" << endl;
+    testing::InitGoogleTest(&argc, argv);
+    return 0;
+}
