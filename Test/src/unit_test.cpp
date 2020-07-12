1d4a39eb1c5aac505837937bae65402b0204e726
djangojiang <djangojiang@tencent.com>
2020-07-12 11:18:09 +0800

    add setup and teardwon testsuite

--- Test/src/unit_test.cpp
+++ Test/src/unit_test.cpp
@@ -4,7 +4,18 @@
 
 #include <iostream>
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 using namespace std;
+
+void f2(){
+    cout<<"f2"<<endl;
+}
+
+int fun1(){
+    f2();
+    cout<<"finish"<<endl;
+    return 0;
+}
 
 class FooTest : public ::testing::Test {
 protected:
@@ -15,7 +26,36 @@ protected:
     void TearDown() override {
         cout<<"TearDown"<<endl;
     }
+
+    static void SetUpTestSuite(){
+        cout<<"setup testsuit"<<endl;
+    }
+
+    static void TearDownTestSuite(){
+        cout <<"teardown testsuite"<<endl;
+    }
 };
+
+class ZooTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        cout<<"SetUp"<<endl;
+    }
+
+    void TearDown() override {
+        cout<<"TearDown"<<endl;
+    }
+
+    static void SetUpTestSuite(){
+        cout<<"setup zoo testsuit"<<endl;
+    }
+
+    static void TearDownTestSuite(){
+        cout <<"teardown zoo testsuite"<<endl;
+    }
+};
+
+
 
 TEST_F(FooTest, testFun) {
     int32_t iRet = 0;
@@ -26,11 +66,19 @@ TEST_F(FooTest, testFun) {
 TEST_F(FooTest, testFun2) {
     int32_t iRet = 1;
 
-    EXPECT_EQ(iRet,0);
+    EXPECT_EQ(iRet,0) << "value is error";
+    ASSERT_EQ(iRet, 0);
 }
 
-int main(int argc, char **argv)
+TEST_F(ZooTest, testFun){
+    int32_t iRet = 0;
+    ASSERT_EQ(iRet,0);
+}
+
+
+
+/*int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
+}*/
