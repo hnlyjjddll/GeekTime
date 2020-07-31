fd7c5927c6a886f38b578ac11bcd30ac53d46923
djangojiang <djangojiang@tencent.com>
2020-07-31 10:11:26 +0800

    建造者模式

---
+++ DesignMode/ConstructMode/construct.cpp
@@ -0,0 +1,62 @@
+//
+// Created by jdl on 2020/7/31.
+//
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+class ServerConfig{
+public:
+    class ServerConfigBuild;
+    const std::string& GetServerName() {
+        return m_strServerName;
+    }
+    int32_t GetMaxNum(){
+        return m_dwMaxNum;
+    }
+    int32_t GetMinNum(){
+        return m_dwMinNum;
+    }
+private:
+    ServerConfig(const ServerConfigBuild& oBuild):m_strServerName(oBuild.m_strServerName),m_dwMaxNum(oBuild.m_dwMaxNum),m_dwMinNum(oBuild.m_dwMinNum){}
+private:
+    std::string m_strServerName;
+    int32_t m_dwMaxNum;
+    int32_t m_dwMinNum;
+public:
+    class ServerConfigBuild{
+    public:
+        friend class ServerConfig;
+        ServerConfigBuild(const std::string& strServerName):m_strServerName(strServerName){}
+        void SetMaxNum(int32_t dwMaxNum){m_dwMaxNum = dwMaxNum;}
+        void SetMinNum(int32_t dwMinNum){m_dwMinNum = dwMinNum;}
+        std::shared_ptr<ServerConfig> Build(){
+            if(m_dwMinNum>m_dwMaxNum) return nullptr;
+
+            return  make_shared<ServerConfig>(ServerConfig(*this));
+        }
+
+    private:
+        std::string m_strServerName;
+        int32_t m_dwMaxNum;
+        int32_t m_dwMinNum;
+    };
+};
+
+int main(void){
+    ServerConfig::ServerConfigBuild oBuildCfg("123");
+    oBuildCfg.SetMaxNum(8);
+    oBuildCfg.SetMinNum(0);
+
+    auto poCfg = oBuildCfg.Build();
+    if(!poCfg){
+        cout<<"build fail"<<endl;
+    }
+
+    cout<<poCfg->GetServerName()<<endl;
+
+    return 0;
+}
--- DesignMode/ConstructMode/factory.cpp
+++ DesignMode/ConstructMode/factory.cpp
@@ -1,4 +1,50 @@
 //
-// Created by djangojiang on 2020/7/30.
+// Created by jdl on 2020/7/30.
 //
 
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Car{
+public:
+    virtual void drive(){cout<<"drive car"<<endl;}
+};
+
+class BusCar:public Car{
+public:
+    void drive() override{cout<<"buscar"<<endl;}
+};
+
+class BycycleCar:public Car{
+public:
+    void drive() override {cout<<"bycyclecar"<<endl;}
+};
+
+class CreateCarFactory{
+public:
+    const static std::map<int,Car*> mapCar;
+    static Car* CreateCar(int type) {
+        if(mapCar.find(type) == mapCar.end()){
+            return nullptr;
+        }
+
+        return mapCar.at(type);
+    }
+};
+
+const map<int,Car*> CreateCarFactory::mapCar = {
+        {1,new BusCar()},
+        {2,new BycycleCar()}
+};
+
+
+int main(void){
+    Car* car = CreateCarFactory::CreateCar(1);
+    car->drive();
+    Car* car2 = CreateCarFactory::CreateCar(2);
+    car2->drive();
+    return 0;
+}
