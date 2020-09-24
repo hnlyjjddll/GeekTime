f537b058fa1905d9f3e1f867ec864cb49e57c8da
hnlyjjddll <18565629271@163.com>
2020-09-24 09:23:48 +0800

    code commit

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -5,119 +5,134 @@
 #include <set>
 #include <map>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
-class Solution {
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  };
+ 
+class Codec {
 public:
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        if(s.empty() || wordDict.empty()){
-            return result;
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root){
+            return "@";
         }
 
-        set<string> wordSet;
-        minLength = INT_MAX;
-        maxLength = 0;
-        for(const auto& word: wordDict){
-            if(word.size() < minLength){
-                minLength = word.size();
+        string str("");
+        queue<TreeNode*> qset;
+        qset.push(root);
+        TreeNode* lastNode = root;
+        while(!qset.empty()){
+            auto pNode = qset.front();
+            qset.pop();
+            if(!pNode){
+                str = (str==""?"":str+"#") + "@";
+                continue;
             }
 
-            if(word.size() > maxLength){
-                maxLength = word.size();
+            str = (str==""?"":str+"#") + to_string(pNode->val);
+            
+            qset.push(pNode->left);
+            qset.push(pNode->right);
+            
+            if(pNode == lastNode && !qset.empty()){
+                lastNode = qset.back();
             }
-
-            wordSet.insert(std::move(word));
-
-            _dfsTraverse(wordSet,s,0,s.size()-1,"");
         }
-        return result;
+
+        int i = str.rfind("@#",0);
+        cout << i<<endl;
+        return str;
     }
 
-private:
-    void _dfsTraverse(const set<string>& wordSet, const string& s,int beg, int end, const string& value){
-        if(mapSet.find(s.substr(beg,end-beg+1)) != mapSet.end()){
-            return;
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.empty()){
+            return nullptr;
         }
 
-        if(wordSet.find(s.substr(beg,end-beg+1)) != wordSet.end()){
-            string tmp = (value==""?"":value + " ") + s.substr(beg,end-beg+1);
-            if(existSet.find(tmp) != existSet.end()){
-                return;
-            }
-            existSet[tmp] = true;
-            result.emplace_back(tmp);
-            //return;
-        }
-
-        int max = end-minLength+1 > beg+maxLength-1 ? beg+maxLength-1 : end-minLength+1;
-        for(int i=beg+minLength-1;i<=max;++i){
-            if(wordSet.find(s.substr(beg,i-beg+1)) != wordSet.end()){
-                string tmp  =  (value==""?"":value+" ") + s.substr(beg,i-beg+1);
-                _dfsTraverse(wordSet,s,i+1,end,tmp);
-            }
-        }
+        vector<string> vecStr;
+        _splitString2Vector(data,vecStr);
+        return _resumeBinaryTree(vecStr);
     }
 private:
-    int minLength;
-    int maxLength;
-    unordered_map<string,bool> mapSet;
-    map<string,bool> existSet;
-    vector<string> result;
+    TreeNode* _resumeBinaryTree(const vector<string>& vecStr){
+        if(vecStr.empty() || vecStr[0] == "@"){
+            return nullptr;
+        }
+
+        TreeNode* root = new TreeNode(stoi(vecStr[0]));
+        queue<TreeNode*> qset;
+        qset.push(root);
+        int curPos = 1;
+        int size = vecStr.size();
+        
+        while(!qset.empty() && curPos<size){
+            auto pNode = qset.front();
+            qset.pop();
+            if(curPos < size){
+                if(vecStr[curPos] != "@"){
+                    pNode->left = new TreeNode(stoi(vecStr[curPos]));
+                    qset.push(pNode->left);
+                }
+                ++curPos;
+                if(curPos < size){
+                    if(vecStr[curPos] != "@"){
+                        pNode->right = new TreeNode(stoi(vecStr[curPos]));
+                        qset.push(pNode->right);
+                    }
+                }
+                ++curPos;
+            }
+        }
+
+        return root;
+    }
+
+    void _splitString2Vector(const string& data, vector<string>& vecStr){
+        int i=0,j=0,size=data.size();
+        for(;j<size;++j){
+            if(data[j] == '#'){
+                vecStr.push_back(data.substr(i,j-i));
+                i = j+1;
+            }
+        }
+
+        vecStr.push_back(data.substr(i,j-i));
+    }
 };
 
+void PrintTreeNode(TreeNode* root){
+    if(!root){
+        return;
+    }
+
+    cout<<root->val<<" ";
+    PrintTreeNode(root->left);
+    PrintTreeNode(root->right);
+}
+
 int main(void){
-    /*Solution s;
-    vector<string> wordSet{"aaaa","aa","a"};
-    vector<string> result = s.wordBreak("aaaaaaa",wordSet);
-    for(const auto& val: result){
-        cout<<val<<endl;
-    }*/
+    Codec s;
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->right->left = new TreeNode(4);
+    root->right->right = new TreeNode(5);
+    
+    string str = s.serialize(root);
+    cout<<"serial string = "<<str<<endl;
 
-    vector<string> src{"aaaa aaaa","aa aa aaaa","aa aa aa aa","aa aaaa aa",
-    "a a a a aaaa","a a a a a a aa","a a a a a a a a","a a a a a aa a",
-    "a a a a aa aa","a a a a aa a a","a a a aa a aa","a a a aa a a a",
-    "a a a aa aa a","a a a aaaa a","a a aa aaaa","a a aa a a aa","a a aa a a a a",
-    "a a aa a aa a","a a aa aa aa","a a aa aa a a","a a aaaa aa",
-    "a a aaaa a a","a aa a aaaa","a aa a a a aa","a aa a a a a a",
-    "a aa a a aa a","a aa a aa aa","a aa a aa a a","a aa aa a aa",
-    "a aa aa a a a","a aa aa aa a","a aa aaaa a","a aaaa a aa",
-    "a aaaa a a a","a aaaa aa a","aa a a aaaa","aa a a a a aa",
-    "aa a a a a a a","aa a a a aa a","aa a a aa aa","aa a a aa a a",
-    "aa a aa a aa","aa a aa a a a","aa a aa aa a","aa a aaaa a"};
-
-    set<string> setSrc;
-    for(const auto& str: src){
-        if(setSrc.find(str) != setSrc.end()){
-            cout<<str<<" repeated"<<endl;
-        }
-
-        setSrc.insert(str);
-    }
-    cout<<"src size: " << setSrc.size()<<endl;
-
-    vector<string> dest{
-        "a a a a a a a a","aa a a a a a a","a aa a a a a a","a a aa a a a a",
-        "aa aa a a a a","aaaa a a a a","a a a aa a a a","aa a aa a a a",
-        "a aa aa a a a","a aaaa a a a","a a a a aa a a","aa a a aa a a",
-        "a aa a aa a a","a a aa aa a a","aa aa aa a a","aaaa aa a a",
-        "a a aaaa a a","aa aaaa a a","a a a a a aa a","aa a a a aa a",
-        "a aa a a aa a","a a aa a aa a","aa aa a aa a","aaaa a aa a",
-        "a a a aa aa a","aa a aa aa a","a aa aa aa a","a aaaa aa a",
-        "a a a aaaa a","aa a aaaa a","a aa aaaa a","a a a a a a aa",
-        "aa a a a a aa","a aa a a a aa","a a aa a a aa","aa aa a a aa",
-        "aaaa a a aa","a a a aa a aa","aa a aa a aa","a aa aa a aa",
-        "a aaaa a aa","a a a a aa aa","aa a a aa aa","a aa a aa aa","a a aa aa aa",
-        "aa aa aa aa","aaaa aa aa","a a aaaa aa","aa aaaa aa","a a a a aaaa",
-        "aa a a aaaa","a aa a aaaa","a a aa aaaa","aa aa aaaa","aaaa aaaa"
-    };
-
-    cout<<"dest size: "<<dest.size()<<endl;
-    for(const auto& str:dest){
-        if(setSrc.find(str) == setSrc.end()){
-            cout<<str<<" not locate dest"<<endl;
-        }
-    }
-
+    cout<<"preorder traverse deserial tree"<<endl;
+    auto pNode = s.deserialize(str);
+    PrintTreeNode(pNode);
+    cout<<endl;
     return 0;
 }
